2195288d4d1d0e520650b116f8111a8963dfb6da
AtillaRustemli <rustemliatilla662@gmail.com>
2023-11-16 23:08:32 +0400

    feat:slider responce

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,12 +1,25 @@
 //------------------new-releases-----------------------
 $(document).ready(function () {
-  $(".new-releases-book").slick({
+  $(".new-releases-books").slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    prevArrow:
+      ' <span class="priv-arrow"><i class="fa-solid fa-angle-left" style="color: #000000;"></i></span>',
+    nextArrow:
+      '<span class="next-arrow"><i class="fa-solid fa-angle-right" style="color: #000000;"></i></span>',
     responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 5,
+          infinite: true,
+          dots: true,
+        },
+      },
       {
         breakpoint: 1024,
         settings: {
