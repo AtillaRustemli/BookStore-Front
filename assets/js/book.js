543ecdf174ac412f8ca7269fb9134b69edc699ea
AtillaRustemli <rustemliatilla662@gmail.com>
2023-11-21 23:27:14 +0400

    feat:trending now

--- assets/js/book.js
+++ assets/js/book.js
@@ -46,44 +46,6 @@ $(document).ready(function () {
     ],
   });
 });
-$(document).ready(function () {
-  $(".trending-now-slider").slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 6,
-    slidesToScroll: 5,
-    prevArrow:
-      '<span class="priv-arrow-trending-now"><i class="fa-solid fa-angle-left" style="color: #000000;"></i></span>',
-    nextArrow:
-      '<span class="next-arrow-trending-now"><i class="fa-solid fa-angle-right" style="color: #000000;"></i></span>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 4,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
-});
 
 //------------------new-releases-----------------------
 //---------------------drawer--------------------------
