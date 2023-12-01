87b7a96d2449951f1cfd616a57a9747a4e824f83
AtillaRustemli <rustemliatilla662@gmail.com>
2023-12-01 23:08:14 +0400

    feat:started new page-Book-detail,Done 10%

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,42 +1,46 @@
 //------------------new-releases-----------------------
-$(document).ready(function () {
-  $(".new-releases-books").slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 7,
-    slidesToScroll: 6,
-    prevArrow:
-      ' <span class="priv-arrow"><i class="fa-solid fa-angle-left" style="color: #000000;"></i></span>',
-    nextArrow:
-      '<span class="next-arrow"><i class="fa-solid fa-angle-right" style="color: #000000;"></i></span>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 4,
-          infinite: true,
-          dots: true,
+try {
+  $(document).ready(function () {
+    $(".new-releases-books").slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 7,
+      slidesToScroll: 6,
+      prevArrow:
+        ' <span class="priv-arrow"><i class="fa-solid fa-angle-left" style="color: #000000;"></i></span>',
+      nextArrow:
+        '<span class="next-arrow"><i class="fa-solid fa-angle-right" style="color: #000000;"></i></span>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true,
+          },
         },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
         },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
         },
-      },
-    ],
+      ],
+    });
   });
-});
+} catch (error) {
+  console.log("An error occurred:", error.message);
+}
 
 //------------------new-releases-----------------------
 //---------------------drawer--------------------------
