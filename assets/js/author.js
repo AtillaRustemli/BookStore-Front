6fba86d14100f27fba6e783010a3a2d69307271c
AtillaRustemli <rustemliatilla662@gmail.com>
2023-12-13 00:41:24 +0400

    feat:author page 50% done

--- assets/js/author.js
+++ assets/js/author.js
@@ -1,8 +1,18 @@
 //-------------------------selected-refine----------------------
 let refine_item = document.querySelectorAll(".refine-child-list");
 function selectedRefine(el) {
-  Array.from(refine_item).forEach((item) => {
-    item.classList.remove(".refine-child-list");
+  let parentEl = el.parentNode;
+  let allChilds = parentEl.getElementsByTagName("li");
+
+  Array.from(allChilds).forEach((item) => {
+    item.classList.remove("active-refine");
   });
-  el.classList.add(".refine-child-list");
+  el.classList.add("active-refine");
+}
+let typeOfBook = document.querySelector(".book-type");
+function selectedBookType(el) {
+  Array.from(typeOfBook.getElementsByTagName("li")).forEach((item) => {
+    item.classList.remove("active-book-type");
+  });
+  el.classList.add("active-book-type");
 }
